596b8f6709f6580c368a3c3c19e7cf4db7975b6e
Edwin Mascarenhas <edwin.mascarenhas95@gmail.com>
2017-10-31 15:54:51 +0100

    resolved cache bugs

--- core/src/cache.cpp
+++ core/src/cache.cpp
@@ -12,19 +12,19 @@ Cache::Cache(Dram* dram){
 		cache[i][1] = 0;
 		cache[i][2] = 0;		
 		cache[i][3] = 0;
-		cache_index[i].tag = 0;
-		cache_index[i].set = 0;
-		cache_index[i].dirtybit=0;
+		index[i].tag = 0;
+		index[i].set = 0;
+		index[i].dirtybit=0;
 	}
 }
 
-void Cache::set(CORE_UINT(32) address, CORE_INT(32) value, CORE_UINT(2) op){
+void Cache::store(CORE_UINT(32) address, CORE_INT(32) value, CORE_UINT(2) op){
 	// For store byte, op = 0
 	// For store half word, op = 1
 	// For store word, op = 3
-	CORE_INT(TAGBITS) tag = getTag(address);
-	CORE_INT(SETBITS) set = getSet(address);
-	CORE_INT(2) offset = getOffset(address);
+	CORE_UINT(TAGBITS) tag = getTag(address);
+	CORE_UINT(SETBITS) set = getSet(address);
+	CORE_UINT(2) offset = getOffset(address);
 	CORE_UINT(8) byte0 = value.SLC(8,0);
 	CORE_UINT(8) byte1 = value.SLC(8,8);
 	CORE_UINT(8) byte2 = value.SLC(8,16);
@@ -33,13 +33,13 @@ void Cache::set(CORE_UINT(32) address, CORE_INT(32) value, CORE_UINT(2) op){
 	CORE_UINT(32) dram_address = 0;
 	dram_address.SET_SLC(0,set);
 	dram_address.SET_SLC(SETBITS,tag);
-	CORE_UINT(1) tag_match = (tag == cache_index[set].tag) ? 1 : 0;
-	
-	if(cache_index[set].dirty_bit == 1 && tag_match == 0){
-		dram.setMemory(dram_address,cache[set][0],0);
-		dram.setMemory(dram_address,cache[set][1],1);
-		dram.setMemory(dram_address,cache[set][2],2);
-		dram.setMemory(dram_address,cache[set][3],3);
+	CORE_UINT(1) tag_match = (tag == index[set].tag) ? 1 : 0;
+	CORE_UINT(1) dirty = index[set].dirtybit;
+	if(dirty && tag_match == 0){
+		dram_location->setMemory(dram_address,cache[set][0],0);
+		dram_location->setMemory(dram_address,cache[set][1],1);
+		dram_location->setMemory(dram_address,cache[set][2],2);
+		dram_location->setMemory(dram_address,cache[set][3],3);
 	}
 
 	cache[set][offset] = byte0;
@@ -50,28 +50,62 @@ void Cache::set(CORE_UINT(32) address, CORE_INT(32) value, CORE_UINT(2) op){
 		cache[set][2] = byte2;
 		cache[set][3] = byte3;
 	}
-	cache_index[set].set = set;
-	cache_index[set].tag = tag;
-	cache_index[set].dirtybit = 1;
+	index[set].set = set;
+	index[set].tag = tag;
+	index[set].dirtybit = 1;
 }
 
-CORE_INT(32) Cache::get(CORE_UINT(32) address, CORE_UINT(2) op, CORE_UINT(1) sign){
+CORE_INT(32) Cache::load(CORE_UINT(32) address, CORE_UINT(2) op, CORE_UINT(1) sign){
 	// For load byte, op = 0
 	// For load half word, op = 1
 	// For load word, op = 3
-	
-	CORE_INT(TAGBITS) tag = getTag(address);
-	CORE_INT(SETBITS) set = getSet(address);
-	CORE_INT(2) offset = getOffset(address);
-	CORE_UINT(1) tag_match = (tag == cache_index[set].tag) ? 1 : 0;
-	
+	CORE_UINT(SETBITS) i;	
+	CORE_UINT(TAGBITS) tag = getTag(address);
+	CORE_UINT(SETBITS) set = getSet(address);
+	CORE_UINT(2) offset = getOffset(address);
+	CORE_UINT(1) tag_match = (tag == index[set].tag) ? 1 : 0;
+	CORE_UINT(1) dirty = index[set].dirtybit;
 	CORE_INT(32) result;
 	result = sign ? -1 : 0;
-	
-	CORE_UINT(8) byte0 = memory[location][0];
-	CORE_UINT(8) byte1 = memory[location][1];
-	CORE_UINT(8) byte2 = memory[location][2];
-	CORE_INT(8) byte3 = memory[location][3];
+	CORE_UINT(32) dram_address = 0;
+	CORE_UINT(8) byte0, byte1, byte2, byte3;
+
+	if(tag_match == 1){
+		byte0 = cache[set][0];
+		byte1 = cache[set][1];
+		byte2 = cache[set][2];
+		byte3 = cache[set][3];
+	}
+	else{
+		//write all dirty blocks to memory
+		for(i = 0;i<SETS;i++){
+			if(index[i].dirtybit == 1){				
+				dram_address = 0;
+				dram_address.SET_SLC(0,i);
+				dram_address.SET_SLC(SETBITS,index[i].tag);
+				dram_location->setMemory(dram_address, cache[i][0], 0);
+				dram_location->setMemory(dram_address, cache[i][1], 1);
+				dram_location->setMemory(dram_address, cache[i][2], 2);
+				dram_location->setMemory(dram_address, cache[i][3], 3);
+			}
+		}
+
+		dram_address = 0;
+		dram_address.SET_SLC(SETBITS,tag);
+		//reading blocks into cache
+		for(i = 0; i< SETS; i++){
+				cache[i][0] = dram_location->getMemory(dram_address,0);
+				cache[i][1] = dram_location->getMemory(dram_address,1);
+				cache[i][2] = dram_location->getMemory(dram_address,2);
+				cache[i][3] = dram_location->getMemory(dram_address,3);
+				dram_address+=4;
+		}
+
+		byte0 = cache[set][0];
+		byte1 = cache[set][1];
+		byte2 = cache[set][2];
+		byte3 = cache[set][3];
+	}
 	switch(offset){
 		case 0:
 			break;
@@ -88,7 +122,7 @@ CORE_INT(32) Cache::get(CORE_UINT(32) address, CORE_UINT(2) op, CORE_UINT(1) sig
 		}
 	result.SET_SLC(0,byte0);
 	if(op & 1){
-		result.SET_SLC(8,byte1);		}
+		result.SET_SLC(8,byte1);
 	}
 	if(op & 2){
 		result.SET_SLC(16,byte2);
