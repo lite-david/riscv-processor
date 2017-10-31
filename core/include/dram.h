b6a8244ba9606b61a13770652fb3ad00de3fac6c
Edwin Mascarenhas <edwin.mascarenhas95@gmail.com>
2017-10-31 15:22:43 +0100

    dram updated

--- core/include/dram.h
+++ core/include/dram.h
@@ -1,18 +1,20 @@
-//vim: set ts=4 nu ai:
+// vim: set ts=4 nu ai:
 #ifndef DRAM_H
 #define DRAM_H
 
 #include <portability.h>
-#include <unordered_map>
+#include <map>
+#include <vector>
 
 class Dram{
 	
 	private:
-		unordered_map<CORE_UINT(32), vector<CORE_UINT(8)> > memory;
+		std::map<CORE_UINT(32), std::vector<CORE_UINT(8)> > memory;
 
 	public:
 		
-		void setMemory(CORE_UINT(32) address, CORE_UINT(2) byte, CORE_UINT(8) value);
+		void setMemory(CORE_UINT(32) address, CORE_UINT(8) value, CORE_UINT(2) byte);
+		void setMemory(CORE_UINT(32) address, CORE_UINT(8) value);
 		CORE_UINT(8) getMemory(CORE_UINT(32) address, CORE_UINT(2) byte);
 
 };
--- core/src/dram.cpp
+++ core/src/dram.cpp
@@ -1,22 +1,31 @@
 // vim: set ts=4 nu ai:
 #include <dram.h>
-#include <vector>
+#include <stdlib.h>
+#include <iostream>
 
 void Dram::setMemory(CORE_UINT(32) address, CORE_UINT(8) value){
 	CORE_UINT(30) location = address >> 2;
-	CORE_UINT(2) byte = address % 4;
+	CORE_UINT(2) byte = address.SLC(2,0);
 	
-	if(memory[location] == 0){
-		memory[location] = vector<CORE_UINT(8)>(4);
+	if(memory[location].size() == 0){
+		memory[location] = std::vector<CORE_UINT(8)>(4,0);
 	}
 	memory[location][byte] = value;		
 }
 
 void Dram::setMemory(CORE_UINT(32) address, CORE_UINT(8) value, CORE_UINT(2) byte){
+	if(memory[address].size() == 0){
+		memory[address] = std::vector<CORE_UINT(8)>(4,0);
+	}
 	memory[address][byte] = value;
 }
 
 CORE_UINT(8) Dram::getMemory(CORE_UINT(32) address, CORE_UINT(2) byte){
+	if(memory[address].size() == 0){
+		std::cout << "Error! Invalid address access in Dram. Terminating" 
+		<< std::endl;
+		exit(-1);
+	}
 	return memory[address][byte];
 }
 
