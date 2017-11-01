5aa8582f966c5a218213afcec3e68bc5d7413f74
Achyudh Ram <edwin.mascarenhas95@gmail.com>
2017-11-01 15:00:23 +0100

    removed .swp files

--- core/unittests/cache_test.cpp
+++ core/unittests/cache_test.cpp
@@ -11,11 +11,19 @@ int main(){
 	Cache cache(&dram);
 	int i=0,value;	
 	//fill the dram
-	for(i=0;i<1000;i++){
+	for(i=0;i<120000;i++){
 		value = rand() % 120;
 		dram.setMemory(i,value);
 	}
-	cout << dram.getMemory(0,0);
-	
+	cout << dram.getMemory(25,0) << endl;
+	cout << dram.getMemory(200,0) << endl;
+	cout << cache.load(100,0,0) << endl;
+	cache.store(300,17,0);
+	cache.store(4,88,0);
+	cout << cache.load(300,0,0) << endl;
+	cout << cache.load(4,0,0) << endl;
+	cout << cache.load(1000,0,0) << endl;
+	cout << dram.getMemory(75,0) << endl;
+	cout << dram.getMemory(1,0) << endl;
 	return 0;
 }
