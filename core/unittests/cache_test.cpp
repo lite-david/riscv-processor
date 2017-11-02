6320c1ba70acf51dc1ced81f794cdab09d801cd2
Edwin Mascarenhas <edwin.mascarenhas95@gmail.com>
2017-11-02 15:41:55 +0100

    cache tests updated

--- core/unittests/cache_test.cpp
+++ core/unittests/cache_test.cpp
@@ -1,9 +1,26 @@
+// vim: set ts=4 nu ai:
 #include <iostream>
 #include <dram.h>
 #include <cache.h>
 #include <stdlib.h>
 
 using namespace std;
+
+void printStats(Cache* cache){
+	cout << "Cache Misses: " << cache->getNumberCacheMiss() << endl;
+	cout << "DRAM reads  : " << cache->getNumberDramReads() << endl;
+	cout << "DRAM writes : " << cache->getNumberDramWrites() << endl;
+	cout << "Loads       : " << cache->getNumberLoads() << endl;
+	cout << "Stores      : " << cache->getNumberStores() << endl;
+}
+
+bool load_test(int address, Cache* cache, Dram* dram){
+	return cache->load(address,0,0) == dram->getMemory(address >> 2, address % 4);
+}
+
+void store_test(int address, Cache* cache, Dram* dram){
+	cache->store(address,46,0);
+}
 
 int main(){
 	
@@ -15,15 +32,20 @@ int main(){
 		value = rand() % 120;
 		dram.setMemory(i,value);
 	}
-	cout << dram.getMemory(25,0) << endl;
-	cout << dram.getMemory(200,0) << endl;
-	cout << cache.load(100,0,0) << endl;
-	cache.store(300,17,0);
-	cache.store(4,88,0);
-	cout << cache.load(300,0,0) << endl;
-	cout << cache.load(4,0,0) << endl;
-	cout << cache.load(1000,0,0) << endl;
-	cout << dram.getMemory(75,0) << endl;
-	cout << dram.getMemory(1,0) << endl;
+	
+	int address[5] = {8,4000,100,2000,5000};
+
+	for(i = 0;i<5;i++){
+		if(!load_test(address[i],&cache,&dram)){
+			cout << "Test " << i << "failed!" << endl;
+		}
+	}
+
+	cache.store(8,10,0);
+	cout << cache.load(8,0,0) << endl;
+	cache.store(4000,22,0);
+	cout << cache.load(4004,0,0) << endl;
+
+	printStats(&cache);
 	return 0;
 }
